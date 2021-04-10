955216ccb0d9db15b114e7e85b407d30781931be
BassCoder2808 <vedant.jolly@spit.ac.in>
2021-04-10 09:47:21 +0530

    Corrected the password

--- se_hackethon_project/__init__.py
+++ se_hackethon_project/__init__.py
@@ -1,3 +1,4 @@
+from se_hackethon_project import routes
 import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
@@ -12,8 +13,8 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
 app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
-app.config['MAIL_USERNAME'] = 'basscoder2808@gmail.com'
-app.config['MAIL_PASSWORD'] = 'VedantJolly'
+app.config['MAIL_USERNAME'] = ''
+app.config['MAIL_PASSWORD'] = ''
 
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
@@ -31,11 +32,8 @@ google = oauth.register(
     authorize_url='https://accounts.google.com/o/oauth2/auth',
     authorize_params=None,
     api_base_url='https://www.googleapis.com/oauth2/v1/',
-    hd = 'spit.ac.in',
-    userinfo_endpoint='https://openidconnect.googleapis.com/v1/userinfo',  # This is only needed if using openId to fetch user info
+    hd='spit.ac.in',
+    # This is only needed if using openId to fetch user info
+    userinfo_endpoint='https://openidconnect.googleapis.com/v1/userinfo',
     client_kwargs={'scope': 'openid email profile'},
 )
-
-from se_hackethon_project import routes
-
-
